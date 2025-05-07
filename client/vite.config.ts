505fbc4834821e61b011339ebf6e7d86768765e0
Zwiswa Muridili <142395130+JepStar990@users.noreply.github.com>
2025-05-08 00:01:29 +0200

    Helper function to resolve paths from import.meta.url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,20 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+const getDirname = (url) => path.dirname(new URL(url).pathname);
+
 export default defineConfig({
   plugins: [
     react(),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      "@": path.resolve(getDirname(import.meta.url), "src"),
+      "@shared": path.resolve(getDirname(import.meta.url), "../shared"),
+      "@assets": path.resolve(getDirname(import.meta.url), "../attached_assets"),
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
+  root: path.resolve(getDirname(import.meta.url), "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(getDirname(import.meta.url), "../dist/public"),
     emptyOutDir: true,
   },
 });
